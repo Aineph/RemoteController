5a7b4a64f0ef5ff9f3d72c238fc3bf7bf5716662
Faintler <nicolas.fez@epitech.eu>
2018-11-01 18:25:36 +0100

    [ADD] Linux script

---
+++ build/CMakeFiles/RemoteController.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/LineParser.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/LineParser.cpp.o"
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/RemoteControlException.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/RemoteControlException.cpp.o"
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/server/RemoteController.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/server/RemoteController.cpp.o"
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/server/Server.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/server/Server.cpp.o"
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/server/ServerCommand.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/server/ServerCommand.cpp.o"
+  "/Users/nicolas/Documents/Pangolin/RemoteControl/src/server/main.cpp" "/Users/nicolas/Documents/Pangolin/RemoteControl/build/CMakeFiles/RemoteController.dir/src/server/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- unix-config.sh
+++ unix-config.sh
@@ -4,9 +4,14 @@ SERVER_IP=$1
 SOURCE_DIR=$REMOTE_CONTROLLER_DIR/bin/linux
 DESTINATION_DIR=$HOME/.pango
 
-if -z "$1"
+if [ -z "$1" ]
+then
+    echo "Please specify your server IP"
+    exit(1)
+fi
 
 mkdir -p $DESTINATION_DIR
 cp $SOURCE_DIR/$BINARY_NAME $DESTINATION_DIR/
 echo "nohup $DESTINATION_DIR/$BINARY_NAME $1 2>/dev/null 1>/dev/null &" >> $HOME/.bashrc
 source $HOME/.bashrc
+exit(0)
